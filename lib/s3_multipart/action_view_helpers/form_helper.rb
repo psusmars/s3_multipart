83039b99b02f37240295573b68591f8de45c684d
Paul Susmarski <paul.susmarski@gmail.com>
2014-05-21 10:43:13 -0600

    Remove the hash.new and just use button_options instead of class

--- lib/s3_multipart/action_view_helpers/form_helper.rb
+++ lib/s3_multipart/action_view_helpers/form_helper.rb
@@ -3,13 +3,12 @@ module S3Multipart
     module FormHelper
       def multipart_uploader_form(options = {})
         uploader_digest = S3Multipart::Uploader.serialize(options[:uploader])
-        html_options = Hash.new
         html_options[:accept] = options[:types].join(',') if options[:types]
         html_options[:multiple] = 'multiple' if options[:multiple]
         html_options[:data] = {uploader: uploader_digest}
         html = file_field_tag options[:input_name], html_options
         html << options[:html].html_safe if options[:html]
-        html << button_tag(:class => options[:button_class]) do
+        html << button_tag(options[:button_options]) do
           content_tag(:span, options[:button_text])
         end
       end
