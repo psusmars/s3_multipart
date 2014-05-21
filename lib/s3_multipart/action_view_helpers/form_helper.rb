fba55ae1b4787ed940f05557bbc0c88e23eb454c
Paul Susmarski <paul.susmarski@gmail.com>
2014-05-21 10:46:21 -0600

    Putback the hash.new and use if/end

--- lib/s3_multipart/action_view_helpers/form_helper.rb
+++ lib/s3_multipart/action_view_helpers/form_helper.rb
@@ -3,11 +3,18 @@ module S3Multipart
     module FormHelper
       def multipart_uploader_form(options = {})
         uploader_digest = S3Multipart::Uploader.serialize(options[:uploader])
-        html_options[:accept] = options[:types].join(',') if options[:types]
-        html_options[:multiple] = 'multiple' if options[:multiple]
+        html_options = Hash.new
+        if options[:types]
+          html_options[:accept] = options[:types].join(',')
+        end
+        if options[:multiple]
+          html_options[:multiple] = 'multiple'
+        end
         html_options[:data] = {uploader: uploader_digest}
         html = file_field_tag options[:input_name], html_options
-        html << options[:html].html_safe if options[:html]
+        if options[:html]
+          html << options[:html].html_safe
+        end
         html << button_tag(options[:button_options]) do
           content_tag(:span, options[:button_text])
         end
