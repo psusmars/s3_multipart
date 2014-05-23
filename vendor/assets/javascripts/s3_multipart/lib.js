46fe7ffbea616304c11984d4ad554e26a376d451
Paul Susmarski <paul.susmarski@gmail.com>
2014-05-23 15:49:39 -0600

    Updates to the x-amz-date renewal calls

--- vendor/assets/javascripts/s3_multipart/lib.js
+++ vendor/assets/javascripts/s3_multipart/lib.js
@@ -78,11 +78,12 @@ function S3MP(options) {
       // activate one of the remaining parts
       if (parts.length) {
         i = _.findIndex(parts, function(el, index, collection) {
-          if (el.status !== "active") {
+          if (el.status !== "active"  && el.status !== "going_to_active") {
             return true;
           }
         });
         if (i !== -1){ 
+          parts[i].status = "going_to_active";
           parts[i].activate();
         }
       }
@@ -130,8 +131,14 @@ function S3MP(options) {
           size = upload.size;
           done = upload.uploaded;
 
-          _.each(upload.inprogress,function(val) {
-            done += val;
+
+          _.each(upload.inprogress,function(val, index) {
+            part_available = _.find(upload.parts, function(part){
+              return part.num == index;
+            });
+
+            if(part_available)
+              done += val;
           });
 
           percent = done/size * 100;
