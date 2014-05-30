c06d1090b9fed22d67f8799312902ba5e4f213a0
Paul Susmarski <paul.susmarski@gmail.com>
2014-05-30 08:25:28 -0600

    Don't use that

--- vendor/assets/javascripts/s3_multipart/lib.js
+++ vendor/assets/javascripts/s3_multipart/lib.js
@@ -480,18 +480,8 @@ UploadPart.prototype.activate = function() {
     this.xhr.open('PUT', 'http://'+this.upload.bucket+'.s3.amazonaws.com/'+this.upload.object_name+'?partNumber='+this.num+'&uploadId='+this.upload.upload_id, true);
     this.xhr.setRequestHeader('x-amz-date', this.date);
     this.xhr.setRequestHeader('Authorization', this.auth);
-    var done;
-    done = false;
-    while(!done)
-    {
-      try
-      {
+
     this.xhr.send(this.blob);
-    } catch(e) {
-      console.log(e);
-    }
-    done = true;
-    }
     this.status = "active";
 
   }
