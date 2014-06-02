b51418c8ffe56d054ae236de9af0c6b6120eb027
Paul Susmarski <paul.susmarski@gmail.com>
2014-06-02 13:34:14 -0600

    Extra bucket by accident

--- lib/s3_multipart/action_view_helpers/form_helper.rb
+++ lib/s3_multipart/action_view_helpers/form_helper.rb
@@ -7,7 +7,7 @@ module S3Multipart
         html_options[:accept] = options[:types].join(',') if options[:types]
         html_options[:multiple] = 'multiple' if options[:multiple]
         html_options[:data] = {uploader: uploader_digest}
-        html = hidden_field_tag("s3_multipart_bucket_name", S3Multipart::Config.instance.bucket.bucket_name)
+        html = hidden_field_tag("s3_multipart_bucket_name", S3Multipart::Config.instance.bucket_name)
         html << file_field_tag(options[:input_name], html_options)
         html << options[:html].html_safe if options[:html]
         html << button_tag(options[:button_options]) do
